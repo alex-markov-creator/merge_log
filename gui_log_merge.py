c9b9b9959e5211f3117e6df16a784c2ea6769360
andrew.bezzubov <agb2019@list.ru>
2021-05-26 18:56:51 +0300

    A graphical interface has been developed to solve a problem of increased complexity

--- gui_log_merge.py
+++ gui_log_merge.py
@@ -29,7 +29,8 @@ log_generator.py <path/to/dir>
     5. кнопка «Запуск», которая осуществляет запуск процесса обработки *.jsonl-файлов
 Графический интерфейс необходимо реализовать с использованием библиотеки PySide2 (либо PySide6).
 """
-import log_merge
+import re
+from dateutil.parser import parse
 import PySide2
 from PySide2 import QtCore, QtWidgets, QtGui
 print(PySide2.__version__)
@@ -38,24 +39,74 @@ print(QtCore.__version__)
 class MyWindow(QtWidgets.QWidget):
     # Определение класса
     def __init__(self, parent=None):
-    # Определяет конструктор класса
+        # Определяет конструктор класса
         QtWidgets.QWidget.__init__(self, parent)
         # Вызывается конструктор базового класса, и ему передается ссылка на родительский компонент.
-        self.btnQuit = QtWidgets.QPushButton("&Запуск")
+        self.btnFile_a = QtWidgets.QPushButton("&Первый файл лога")
+        self.btnFile_b = QtWidgets.QPushButton("&Второй файл лога")
+        self.text = QtWidgets.QTextEdit()
+        self.btnRun = QtWidgets.QPushButton("&Запуск")
         self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addWidget(self.btnQuit)
+        self.vbox.addWidget(self.btnFile_a)
+        self.vbox.addWidget(self.btnFile_b)
+        self.vbox.addWidget(self.text)
+        self.vbox.addWidget(self.btnRun)
         self.setLayout(self.vbox)
-        self.btnQuit.clicked.connect(self.run)# Обработчик сигнала
+        self.btnFile_a.clicked.connect(self.path_file_a)# Обработчик сигнала
+        self.btnFile_b.clicked.connect(self.path_file_b)# Обработчик сигнала
+        self.btnRun.clicked.connect(self.run)# Обработчик сигнала
 
     @QtCore.Slot()
     def run(self):
-        print('hi!')
+        self.file_a = ''.join(self.infile_a)
+        self.file_b = ''.join(self.infile_b)
+        self.file_out = 'gui_log_c.jsonl'
+        for infile in self.file_a,self.file_b:
+            with open(infile, 'r') as file:
+                with open(self.file_out, 'a+') as write_file:
+                    for line in file:
+                        write_file.write(line)
+        with open(self.file_out, 'a+') as f_in:
+            print(*sorted(f_in.readlines(), key=lambda x: parse(re.search(r'(?<=")\d[-:\d ]+', x).group())))
+        self.text.setText('gui_log_c.jsonl')
+
+    @QtCore.Slot()
+    def path_file_a(self):
+        dialog = QtWidgets.QFileDialog(parent=window,
+                               filter="Logs (*.jsonl)",
+                               caption="Путь к файл")
+        dialog.setAcceptMode(QtWidgets.QFileDialog.AcceptOpen)
+        dialog.setDirectory(QtCore.QDir.currentPath())
+
+        result = dialog.exec()
+        if result == QtWidgets.QDialog.Accepted:
+            self.infile_a = dialog.selectedFiles()
+            self.text.setText(''.join(self.infile_a))
+            return self.infile_a
+        else:
+            print("Нажата кнопка Cancel")
+
+    @QtCore.Slot()
+    def path_file_b(self):
+        dialog = QtWidgets.QFileDialog(parent=window,
+                               filter="Logs (*.jsonl)",
+                               caption="Путь к файл")
+        dialog.setAcceptMode(QtWidgets.QFileDialog.AcceptOpen)
+        dialog.setDirectory(QtCore.QDir.currentPath())
+
+        result = dialog.exec()
+        if result == QtWidgets.QDialog.Accepted:
+            self.infile_b = dialog.selectedFiles()
+            self.text.setText(''.join(self.infile_b))
+            return self.infile_b
+        else:
+            print("Нажата кнопка Cancel")
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()   # Создаем экземпляр класса
-    window.setWindowTitle("ООП-стиль создания окна")
+    window.setWindowTitle("Логи в один файл")
     window.resize(300, 70)
     window.show()         # Отображаем окно
     sys.exit(app.exec_()) # Запускаем цикл обработки событий
